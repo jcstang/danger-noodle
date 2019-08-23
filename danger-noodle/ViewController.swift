75e9e3c2bd6d1879b569035d41ebfb81983dd086
devJake <devjake@outlook.com>
2019-08-23 09:08:02 -0600

    Initial Commit

---
+++ danger-noodle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  danger-noodle
+//
+//  Created by Jake Stanger on 8/23/19.
+//  Copyright © 2019 Jake Stanger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
